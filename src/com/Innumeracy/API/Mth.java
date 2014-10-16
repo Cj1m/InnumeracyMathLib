85dd20f636552e462b30b45666144d0631bfbac7
cj1m <cj1mckay@gmail.com>
2014-10-16 22:18:52 +0100

    isPrime() is now efficient

--- src/com/Innumeracy/API/Mth.java
+++ src/com/Innumeracy/API/Mth.java
@@ -5,7 +5,9 @@ import java.util.ArrayList;
 public class Mth {
 	// Booleans
 	/**
-	 * @param number CShecked for being palindromic (looking the same when written backwards). Examples of palindromes are 101 or 234432.
+	 * @param number
+	 *            CShecked for being palindromic (looking the same when written
+	 *            backwards). Examples of palindromes are 101 or 234432.
 	 * @return true if number is palindromic, false otherwise.
 	 */
 	public static boolean isPalindromic(int number) {
@@ -16,15 +18,23 @@ public class Mth {
 	}
 
 	/**
-	 * @param number Checked for being prime (only divisible by itself and 1).
+	 * @param number
+	 *            Checked for being prime (only divisible by itself and 1).
 	 * @return true if number is prime, false otherwise.
 	 */
 	public static boolean isPrime(int number) {
 		if (number < 0) {
-			throw new IllegalArgumentException();
+			throw new IllegalArgumentException("Error: int < 0!");
 		}
-		int half = Math.round(number / 2);
-		for (int j = half; j >= 2; j--) {
+		int sqrt = (int) (Math.sqrt(number) + 1);
+
+		if (number == 2) {
+			return true;
+		}
+		if (number == 1) {
+			return false;
+		}
+		for (int j = 2; j <= sqrt; j += 1) {
 			if (number % j == 0) {
 				return false;
 			}
@@ -34,12 +44,14 @@ public class Mth {
 	}
 
 	/**
-	 * @param number Checked for being square (a product of a number multiplied by itself).
+	 * @param number
+	 *            Checked for being square (a product of a number multiplied by
+	 *            itself).
 	 * @return true if number is square, false otherwise.
 	 */
 	public static boolean isSquare(int number) {
 		if (number < 0) {
-			throw new IllegalArgumentException();
+			throw new IllegalArgumentException("Error: int < 0!");
 		}
 
 		if (Math.sqrt(number) % 1 == 0) {
@@ -51,27 +63,33 @@ public class Mth {
 
 	// Doubles
 	/**
-	 * @param number the decimal number which is rounded to 'decimalPlaces' amount of decimal places.
-	 * @param decimalPlaces the number of decimal places 'number' is rounded to.
+	 * @param number
+	 *            the decimal number which is rounded to 'decimalPlaces' amount
+	 *            of decimal places.
+	 * @param decimalPlaces
+	 *            the number of decimal places 'number' is rounded to.
 	 * @return the rounded decimal number.
 	 */
 	public static double roundToDecimalPlaces(double number, int decimalPlaces) {
 		if (decimalPlaces < 0) {
-			throw new IllegalArgumentException();
+			throw new IllegalArgumentException("Error: int < 0!");
 		}
-		double roundedNumber = (double) Math.round(number * power(10, decimalPlaces))
+		double roundedNumber = (double) Math.round(number
+				* power(10, decimalPlaces))
 				/ power(10, decimalPlaces);
 		return roundedNumber;
 	}
 
 	/**
-	 * @param number the base in the Exponentiation operation.
-	 * @param power the mantissa in the Exponentiation operation.
+	 * @param number
+	 *            the base in the Exponentiation operation.
+	 * @param power
+	 *            the mantissa in the Exponentiation operation.
 	 * @return 'number' * 'number' 'power' amount of times.
 	 */
 	public static double power(double number, int power) {
 		if (power < 0) {
-			throw new IllegalArgumentException();
+			throw new IllegalArgumentException("Error: int < 0!");
 		}
 		double answer = 1;
 		for (int j = 0; j < power; j++) {
@@ -82,18 +100,19 @@ public class Mth {
 
 	// Ints
 	/**
-	 * @param number the number whose factors are desired.
+	 * @param number
+	 *            the number whose factors are desired.
 	 * @return int[] of all the factors of 'number'.
 	 */
 	public static int[] factorsOf(int number) {
 		if (number < 0) {
-			throw new IllegalArgumentException();
+			throw new IllegalArgumentException("Error: int < 0!");
 		}
 
 		int half = Math.round(number / 2);
 		ArrayList<Integer> factorsList = new ArrayList<Integer>();
 		for (int j = 1; j <= half; j++) {
-			if (number % j == 0){
+			if (number % j == 0) {
 				factorsList.add(j);
 			}
 		}
@@ -105,21 +124,26 @@ public class Mth {
 	}
 
 	/**
-	 * @param number1 the first number.
-	 * @param number2 the second number.
-	 * @return the Greatest Common Divisor of both 'number1' and 'number2' using Euclid's algorithm.
+	 * @param number1
+	 *            the first number.
+	 * @param number2
+	 *            the second number.
+	 * @return the Greatest Common Divisor of both 'number1' and 'number2' using
+	 *         Euclid's algorithm.
 	 */
 	public static int gcd(int number1, int number2) {
-		if(number2 == 0){
+		if (number2 == 0) {
 			return number1;
 		}
-		
+
 		return gcd(number2, number1 % number2);
 	}
 
 	/**
-	 * @param number1 the first number.
-	 * @param number2 the second number.
+	 * @param number1
+	 *            the first number.
+	 * @param number2
+	 *            the second number.
 	 * @return the lowest common multiple of both 'number1' and 'number2'.
 	 */
 	public static int lcm(int number1, int number2) {
@@ -127,8 +151,10 @@ public class Mth {
 	}
 
 	/**
-	 * @param number whose digits are counted.
-	 * @return the number of digits in the number E.g 3.14 = 3 digits (3,1 and 4).
+	 * @param number
+	 *            whose digits are counted.
+	 * @return the number of digits in the number E.g 3.14 = 3 digits (3,1 and
+	 *         4).
 	 */
 	public static int getNumberOfDigits(double number) {
 		String digits = Double.toString(number);
@@ -142,7 +168,8 @@ public class Mth {
 
 	// String
 	/**
-	 * @param fraction which to be 'simplified'
+	 * @param fraction
+	 *            which to be 'simplified'
 	 * @return 'simplified' fraction as a String E.g "2/4" = "1/2"
 	 */
 	public static String simplifyFraction(String fraction) {
